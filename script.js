851ddd2a9cd3194ce867e77ef53eddac2eb839db
djuodis <deividas73245@gmail.com>
2023-11-19 05:57:06 +0200

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// 1. Užduotis
+
+ const vardas = 'Deividas'.length
+ vardas < 5 ? console.log('Short Name') : console.log('Long Name')
+
+
+// 2. Užduotis
+
+ const clientAge = -1
+ const legalAge = 18 
+ clientAge > legalAge ? alert ('Can Drive') : alert ('Cant drive')
+
+ 
+// 3. Užduotis
+ 
+clientAge > 0 || clientAge < 120 ? alert('Invalid Age') :
+
+clientAge >= legalAge ? alert ('Can Drive') : 
+
+alert ('Cant drive')
+
+
+// 4. Užduotis
+
+const phone = ('Iphone')
+const isIphoneUser = phone === 'Iphone'
